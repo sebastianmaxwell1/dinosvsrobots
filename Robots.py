39d9e4752e7e9f52bd138f5fdc75d7730e75c292
Sebastian Maxwell <maxwellsebas@gmail.com>
2021-03-19 14:00:49 -0400

    Added methods to Robot class, will not run and has no error messages.

--- Robots.py
+++ Robots.py
@@ -1,19 +1,31 @@
 class IanMalcolmTheDinosaurDestroyer:
     def __init__(self):
+        self.name = "Ian Malcolm The Dinosaur Destroyer"
         self.health_status = 100
         self.power_level = ''
         self.weapon_is_a_computer_screen = True
         self.primary_attack = 'JavaScript Aneurysm'
         self.secondary_attack = 'Bores opponent to death by showing Packers footage'
 
-    def check_health_status(self):
+    def check_health_status(self, health):
         response = input('What is the Ian Malcolm dinosaur destroyers health percentage?')
-        if self.health_status:
-            var = self.health_status != 100
-            print(f'Better suit up with new armor quick!')
+        if health >= 100:
+            print(f'{self.name}Better suit up with new armor quick! Your health is at:{self.health_status}')
         elif self.health_status:
             var = self.health_status == 100
             print("The Ian Malcolm dinosaur destroyer is healthy and ready for battle!")
+
+    def attack_dinosaur(self):
+        response = input('Which attack would you like to preform? Primary or Secondary?')
+        if input() == self.primary_attack:
+            print("You have attacked with JavaScript aneurysm! Your opponent is down and out!")
+        elif input() == self.secondary_attack:
+            print(f'You have attacked with{self.secondary_attack}')
+
+
+
+
+
 
 
 
--- main.py
+++ main.py
@@ -2,4 +2,3 @@ from Robots import IanMalcolmTheDinosaurDestroyer
 
 if __name__ == '__main__':
     my_robot = IanMalcolmTheDinosaurDestroyer()
-    my_robot.check_health_status()
