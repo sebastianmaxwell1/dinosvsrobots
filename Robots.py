75e6ba21894dbcedcd4900134be8ef26632c362a
Sebastian Maxwell <maxwellsebas@gmail.com>
2021-03-19 00:46:35 -0400

    Created first robot class, not  running correctly at if elif statement

---
+++ Robots.py
@@ -0,0 +1,21 @@
+class IanMalcolmTheDinosaurDestroyer:
+    def __init__(self):
+        self.health_status = 100
+        self.power_level = ''
+        self.weapon_is_a_computer_screen = True
+        self.primary_attack = 'JavaScript Aneurysm'
+        self.secondary_attack = 'Bores opponent to death by showing Packers footage'
+
+    def check_health_status(self):
+        response = input('What is the Ian Malcolm dinosaur destroyers health percentage?')
+        if self.health_status:
+            var = self.health_status != 100
+            print(f'Better suit up with new armor quick!')
+        elif self.health_status:
+            var = self.health_status == 100
+            print("The Ian Malcolm dinosaur destroyer is healthy and ready for battle!")
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-
-
-
-
+from Robots import IanMalcolmTheDinosaurDestroyer
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    my_robot = IanMalcolmTheDinosaurDestroyer()
+    my_robot.check_health_status()
