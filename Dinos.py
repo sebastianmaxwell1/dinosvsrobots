6ae88dc3bcb93375cf99115c9c8db404a883207a
Sebastian <maxwellsebas@gmail.com>
2021-06-11 17:23:50 -0400

    Simplified dino file, had way to much going on there. runs smoothly

--- Dinos.py
+++ Dinos.py
@@ -1,23 +1,40 @@
-class Dinosaurs:
-    def __init__(self):
-        self.dino_type = 'Velociraptor'
-        self.dino_health = ''
-        self.dino_energy = ''
-        self.dino_weapon = 'claws, bite'
-        self.dino_attack_power = ''
+class Dino:
+    def __init__(self, dino, power, attack):
+        self.type = dino
+        self.health = 100
+        self.power = power
+        self.attack = attack
 
-    def dino_species(self):
-        response = input(f'Your robots name is {self.dino_type}')
+    def dino_attack(self, robot):
+        robot.robos_health -= self.dinos_attack
 
-    def dino_stats(self):
-        if self.dino_weapon == 1:
-            self.dino_health = 60
-            self.dino_energy = 60
-            self.dino_attack_power = 45
-            print(f'Your dinosaurs health is {self.dino_health}, Claw Attack level is {self.dino_attack_power}')
-        elif self.dino_weapon == 2:
-            self.dino_health = 60
-            self.dino_energy = 60
-            self.dino_attack_power = 50
-            print(f' Your dinosaurs health is {self.dino_health}, Bite Attack level is {self.dino_attack_power}')
+
+# ////////////////////////////////////////////////////
+# class Dino:
+#     def __init__(self):
+#         self.dino_type = 'Velociraptor'
+#         self.dino_health = ''
+#         self.dino_energy = ''
+#         self.dino_weapon = 'claws, bite'
+#         self.dino_attack_power = ''
+
+#     def dino_species(self):
+#         response = input(f'Your robots name is {self.dino_type}')
+
+#     def dino_stats(self):
+#         if self.dino_weapon == 1:
+#             self.dino_health = 60
+#             self.dino_energy = 60
+#             self.dino_attack_power = 45
+#             print(f'Your dinosaurs health is {self.dino_health}, Claw Attack level is {self.dino_attack_power}')
+#         elif self.dino_weapon == 2:
+#             self.dino_health = 60
+#             self.dino_energy = 60
+#             self.dino_attack_power = 50
+#             print(f' Your dinosaurs health is {self.dino_health}, Bite Attack level is {self.dino_attack_power}')
+
+
+#////////////////////////////////////////////////////
+
+
 
