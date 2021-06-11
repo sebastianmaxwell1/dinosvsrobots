f86c09885d903e1bac0320833b864d44ceac5cb7
Sebastian <maxwellsebas@gmail.com>
2021-06-11 17:26:56 -0400

    created robot fleet

---
+++ Fleet.py
@@ -0,0 +1,7 @@
+from robot import Robot
+from weapon import Weapon
+
+class Fleet:
+
+    def __init__(self):
+        self.fleet = [Robot("Terminator", 100, Weapon("shotgun"), 20), Robot("iron giant", 100,Weapon("size and strength"), 40), Robot("Iron man", 100, Weapon("Rockets"), 50)]
